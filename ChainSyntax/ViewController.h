3a6391ac1b337770948ac4c3f69a82df9b74d5fc
xuym <m15913143122@163.com>
2016-11-29 10:33:16 +0800

    Initial Commit

---
+++ ChainSyntax/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChainSyntax
+//
+//  Created by jack xu on 16/11/29.
+//  Copyright © 2016年 jack xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ChainSyntax/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ChainSyntax
+//
+//  Created by jack xu on 16/11/29.
+//  Copyright © 2016年 jack xu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
